ac8f97f624e281969ef7feebfb50b6c5e5533c1b
dannyarena <arenadanny71@gmail.com>
2025-04-11 16:48:30 +0200

    Add functionality to display generated random numbers in the list

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -7,3 +7,10 @@ const message = document.getElementById('message');
 
 const randomNumbers = generateRandomNumbers(5, 1, 50);
 console.log("Numeri generati:", randomNumbers);
+
+randomNumbers.forEach(num => {
+    const li = document.createElement('li');  // crea un <li>
+    li.textContent = num;                     // ci scrive dentro il numero
+    numbersList.appendChild(li);              // lo aggiunge alla lista
+  });
+  
