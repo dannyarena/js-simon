5c2fb0a97f394d00924e91c9ccf4baedd598136b
dannyarena <arenadanny71@gmail.com>
2025-04-11 16:45:51 +0200

    Add random number generation and logging

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -3,3 +3,7 @@ const countdownElement = document.getElementById('countdown');
 const form = document.getElementById('answers-form');
 const inputGroup = document.getElementById('input-group');
 const message = document.getElementById('message');
+
+
+const randomNumbers = generateRandomNumbers(5, 1, 50);
+console.log("Numeri generati:", randomNumbers);
