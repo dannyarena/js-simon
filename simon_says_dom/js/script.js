6b4feb8175842cc84ce5589d10edb0fe54185e8c
dannyarena <arenadanny71@gmail.com>
2025-04-11 16:41:43 +0200

    Initialize main variables

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,5 @@
+const numbersList = document.getElementById('numbers-list');
+const countdownElement = document.getElementById('countdown');
+const form = document.getElementById('answers-form');
+const inputGroup = document.getElementById('input-group');
+const message = document.getElementById('message');
